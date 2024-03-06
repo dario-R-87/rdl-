8b730b94d9956041b4df612f11d283ab0396da5a
Dario Russo <dariorusso987@gmail.com>
2024-03-06 12:21:06 +0100

    add delete and update function

--- client/src/components/document/CreateDocument.js
+++ client/src/components/document/CreateDocument.js
@@ -4,9 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import RowsList from './RowsList';
 import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 
 const CreateDocument = () => {
+
+    const navigate = useNavigate();
 
     const getCurrentDate = () => {
         const today = new Date();
@@ -24,11 +27,9 @@ const CreateDocument = () => {
         quanti: 1,
         codmat: ''
     });
-
     const [rows, setRows] = useState([]);
-
     const [isTestataSave, setIsTestataSave] = useState(false)
-
+    const [update, setUpdate] = useState({ updating: false, rownum: 0 })
     const [newSerial, setNewSerial] = useState('0000000001')
 
     useEffect(() => {
@@ -59,43 +60,83 @@ const CreateDocument = () => {
         });
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Qui puoi gestire l'invio dei dati, ad esempio inviandoli al server
-        if (rows.length === 0)
-            alert("Nessura riga inserita");
-        else {
-            rows.forEach(async (row) => {
-                try {
-                    const response = await fetch("http://192.168.1.29:5000/record/add", {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                        body: JSON.stringify(row),
-                    });
-                    if (!response.ok) {
-                        const message = `An error occurred: ${response.statusText}`;
-                        window.alert(message);
-                        return;
-                    }
-                    setFormData({
-                        tipdoc: 'INVEN',
-                        datadoc: getCurrentDate(),
-                        codart: '',
-                        unimis: 'n.',
-                        quanti: 1,
-                        codmat: ''
-                    });
-                    setIsTestataSave(false)
-                    setRows([]);
-                    alert("Documento Creato!");
-                } catch (error) {
-                    console.error("Errore nella richiesta post:", error);
-                    window.alert(error);
-                    return;
-                };
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     // Qui puoi gestire l'invio dei dati, ad esempio inviandoli al server
+    //     if (rows.length === 0)
+    //         alert("Nessura riga inserita");
+    //     else {
+    //         rows.forEach(async (row) => {
+    //             try {
+    //                 const response = await fetch("http://192.168.1.29:5000/record/add", {
+    //                     method: "POST",
+    //                     headers: {
+    //                         "Content-Type": "application/json",
+    //                     },
+    //                     body: JSON.stringify(row),
+    //                 });
+    //                 if (!response.ok) {
+    //                     const message = `An error occurred: ${response.statusText}`;
+    //                     window.alert(message);
+    //                     return;
+    //                 }
+    //                 setFormData({
+    //                     tipdoc: 'INVEN',
+    //                     datadoc: getCurrentDate(),
+    //                     codart: '',
+    //                     unimis: 'n.',
+    //                     quanti: 1,
+    //                     codmat: ''
+    //                 });
+    //                 setIsTestataSave(false)
+    //                 setRows([]);
+    //                 alert("Documento Creato!");
+    //             } catch (error) {
+    //                 console.error("Errore nella richiesta post:", error);
+    //                 window.alert(error);
+    //                 return;
+    //             };
+    //         });
+    //         navigate('/');
+    //     }
+    // };
+    const postData = async (row) => {
+        try {
+            const response = await fetch("http://192.168.1.29:5000/record/add", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(row),
             });
+            if (!response.ok) {
+                throw new Error(`An error occurred: ${response.statusText}`);
+            }
+        } catch (error) {
+            console.error("Errore nella richiesta post:", error);
+            throw error; // Rilancia l'errore per gestirlo nell'handler esterno
+        }
+    };
+    
+    const handleSubmit = async () => {
+        try {
+            await Promise.all(rows.map(postData)); // Esegue tutte le richieste in parallelo
+            // Se tutte le richieste sono state completate con successo
+            setFormData({
+                tipdoc: 'INVEN',
+                datadoc: getCurrentDate(),
+                codart: '',
+                unimis: 'n.',
+                quanti: 1,
+                codmat: ''
+            });
+            setIsTestataSave(false);
+            setRows([]);
+            alert("Documento Creato!");
+            navigate('/');
+        } catch (error) {
+            // Se una o più richieste hanno fallito, gestisci l'errore qui
+            window.alert(error.message);
         }
     };
 
@@ -128,11 +169,63 @@ const CreateDocument = () => {
             setIsTestataSave(true)
     }
 
+    const onDelete = (rownum) => {
+        // Filtra gli elementi escludendo quello con rownum uguale a rownumToDelete
+        const updatedRows = rows.filter(row => row.rownum !== rownum);
+        // Aggiorna lo stato con il nuovo array di elementi
+        setRows(updatedRows);
+        resetByUpdate();
+        alert("Riga eliminata");
+    }
+
+    const onUpdate = (rownum) => {
+        setUpdate({ ...update, updating: true, rownum: rownum })
+        const rowByUpdate = rows.find(row => row.rownum === rownum);
+        setFormData({
+            ...formData,
+            codmat: rowByUpdate.codmat,
+            quanti: rowByUpdate.quanti,
+            codart: rowByUpdate.codart,
+        })
+    }
+
+    const resetByUpdate = () => {
+        setFormData({
+            ...formData,
+            quanti: 1,
+            codmat: '',
+            codart: '',
+        });
+        setUpdate({...update, updating: false, rownum: 0})
+    }
+
+    const updateRow = () => {
+        // Crea un nuovo array aggiornando l'elemento con il rownum corrispondente
+        const updatedRows = rows.map(row => {
+            // Se rownum corrisponde, aggiorna l'elemento con i dati da formData
+            if (row.rownum === update.rownum) {
+                return {
+                    ...row,
+                    codmat: formData.codmat,
+                    quanti: formData.quanti,
+                    codart: formData.codart
+                };
+            }
+            // Altrimenti, lascia l'elemento invariato
+            return row;
+        });
+        // Aggiorna lo stato con il nuovo array di elementi
+        setRows(updatedRows);
+        // Resetta il formData
+        resetByUpdate();
+        alert("Riga Aggiornata");
+    }
+
     return (
         <Container className='mt-3'>
-            <div className='d-flex justify-content-between'>
-                <h2>Nuovo Documento</h2>
+            <div className='my-3 d-flex justify-content-between'>
                 <Link to="/"><Button variant='secondary'>Home</Button></Link>
+                <h2>Nuovo Documento</h2>
             </div>
             <Form onSubmit={addRowHandler}>
                 <Form.Group controlId="tipdoc">
@@ -175,14 +268,20 @@ const CreateDocument = () => {
                 </Form.Group>
 
                 <div className='d-flex justify-content-between'>
-                    <Button className="mt-3" variant="primary" type="submit">
+                    {!update.updating && <Button className="mt-3" variant="primary" type="submit">
                         Aggiungi Riga
-                    </Button>
-                    <Button onClick={handleSubmit} className="mt-3" variant="warning" type="">
+                    </Button>}
+                    {update.updating && <Button onClick={() => { updateRow() }} className="mt-3" variant="success">
+                        Salva Modifiche
+                    </Button>}
+                    {!update.updating && <Button onClick={handleSubmit} className="mt-3" variant="warning" type="">
                         Salva Documento
-                    </Button>
+                    </Button>}
+                    {update.updating && <Button onClick={()=>resetByUpdate()} className="mt-3" variant="danger">
+                        Annulla
+                    </Button>}
                 </div>
-            <RowsList rows={rows} />
+                <RowsList rows={rows} handleDelete={onDelete} handleUpdate={onUpdate} />
             </Form>
         </Container>
     );
--- client/src/components/document/RowsList.js
+++ client/src/components/document/RowsList.js
@@ -4,11 +4,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenToSquare,faTrash } from '@fortawesome/free-solid-svg-icons';
 
 
-const RowsList = ({ rows, handleDelete }) => {
+const RowsList = ({ rows, handleDelete, handleUpdate }) => {
    
-    const rowsReverse = rows.reverse()
     return (
-        <Container className='mt-3'>
+        <div className='mt-3'>
             {rows.slice().reverse().map((row) => (
                 <Card key={row.rownum} className="mb-3 bg-primary text-white">
                     <Card.Body>
@@ -20,14 +19,14 @@ const RowsList = ({ rows, handleDelete }) => {
                                 </Card.Text>
                             </div>
                             <div className="d-flex flex-column align-items-start gap-1">
-                                <Button variant="primary"><FontAwesomeIcon icon={faPenToSquare} /></Button>
-                                <Button variant="danger" className="me-2" onClick={() => handleDelete(row.rownum)}><FontAwesomeIcon icon={faTrash} /></Button>
+                                <Button variant="primary" onClick={()=>handleUpdate(row.rownum)}><FontAwesomeIcon icon={faPenToSquare} /></Button>
+                                <Button variant="danger" className="me-2" onClick={()=>handleDelete(row.rownum)}><FontAwesomeIcon icon={faTrash} /></Button>
                             </div>
                         </div>
                     </Card.Body>
                 </Card>
             ))}
-        </Container>
+        </div>
     )
 }
 
