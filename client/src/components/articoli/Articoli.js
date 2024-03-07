a0dd1aa8b3233a67dea15c0f7d26198f38a47889
Dario Russo <dariorusso987@gmail.com>
2024-03-07 14:49:29 +0100

    fix filter

--- client/src/components/articoli/Articoli.js
+++ client/src/components/articoli/Articoli.js
@@ -1,14 +1,36 @@
 import React, { useState, useEffect } from 'react';
-import { Modal, Button, Card } from 'react-bootstrap';
-import LoadSpinner from "../loading/LoadSpinner"
+import { Modal, Button, Card, Form } from 'react-bootstrap';
+import LoadSpinner from "../loading/LoadSpinner";
+
 
 const Articoli = ({ show, handleClose, handleArticoloSelect, searchValue }) => {
     const [articoli, setArticoli] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [filterValue, setFilterValue] = useState(searchValue);
+    const [artsFiltered, setArtsFiltered] = useState([]);
+
+    const handleChange = (e) => {
+        const newValue = e.target.value
+        setFilterValue(newValue)
+    }
+
+    const filterArt = () => {
+        // console.log("filter art "+filterValue);
+        // console.log(articoli)
+        const arts = articoli.filter((item) => {
+            return item.CADESART.toLowerCase().includes(filterValue.toLowerCase())
+        });
+        setArtsFiltered(arts);
+    }
+
+    useEffect(() => {
+        //if(filterValue.length>4 || filterValue.length===0)
+        filterArt();
+    }, [filterValue]);
 
     // This method fetches the records from the database.
     const getArticoli = async () => {
-        try {console.log("eseguo fetch")
+        try {
             setLoading(true);
             const response = await fetch("http://localhost:5000/articoli");
             if (!response.ok) {
@@ -17,10 +39,11 @@ const Articoli = ({ show, handleClose, handleArticoloSelect, searchValue }) => {
                 return;
             }
             let records = await response.json();
-            if(searchValue.length>4){
-                records = records.filter((item) => {
-                    return item.CADESART.toLowerCase().includes(searchValue.toLowerCase())
+            if (filterValue.length > 0) {
+                const filteredRecords = records.filter((item) => {
+                    return item.CADESART.toLowerCase().includes(filterValue.toLowerCase())
                 });
+                setArtsFiltered(filteredRecords)
             }
             setArticoli(records);
             setLoading(false);
@@ -47,7 +70,29 @@ const Articoli = ({ show, handleClose, handleArticoloSelect, searchValue }) => {
             </Modal.Header>
             <Modal.Body className="row justify-content-center mx-3">
                 {loading && <LoadSpinner />}
-                {!loading && articoli.map((articolo) => (
+                {!loading &&
+                    <Form>
+                        <Form.Group controlId="codart">
+                            <Form.Label className='custom-label mt-3'></Form.Label>
+                            <div className='d-flex'>
+                                <Form.Control placeholder="Cerca..." type="text" name="search" value={filterValue} onChange={handleChange} />
+                            </div>
+                        </Form.Group>
+                        {/* <div className='d-flex justify-content-between'>
+                            {!update.updating && <Button className="mt-3" variant="primary" type="submit">
+                                Aggiungi Riga
+                            </Button>}
+                        </div> */}
+                    </Form>}
+                {!loading && artsFiltered.length>0 && artsFiltered.map((articolo) => (
+                    <Card className="mt-2" key={articolo.CACODICE} onClick={() => selectArticolo(articolo.CACODART)} style={{ cursor: 'pointer' }}>
+                        <Card.Body>
+                            <Card.Title>{articolo.CACODICE}</Card.Title>
+                            <Card.Text>{articolo.CADESART + articolo.CADESSUP}</Card.Text>
+                        </Card.Body>
+                    </Card>
+                ))}
+                {!loading && artsFiltered.length===0 && articoli.map((articolo) => (
                     <Card className="mt-2" key={articolo.CACODICE} onClick={() => selectArticolo(articolo.CACODART)} style={{ cursor: 'pointer' }}>
                         <Card.Body>
                             <Card.Title>{articolo.CACODICE}</Card.Title>
--- client/src/components/document/CreateDocument.js
+++ client/src/components/document/CreateDocument.js
@@ -27,7 +27,7 @@ const CreateDocument = () => {
         codart: '',
         unimis: 'n.',
         quanti: 1,
-        codmat: 'NULL',
+        codmat: null,
         search: '',
         desc: ''
     });
@@ -60,57 +60,12 @@ const CreateDocument = () => {
 
     const handleChange = (e) => {
         const { name, value } = e.target;
-        // if(name==='codart'){
-        //     if(value.length>4 && value.length>formData[name].length){
-        //         setShow(true)
-        //     }
-        // }
         setFormData({
             ...formData,
             [name]: value
         });
     };
 
-    // const handleSubmit = (e) => {
-    //     e.preventDefault();
-    //     // Qui puoi gestire l'invio dei dati, ad esempio inviandoli al server
-    //     if (rows.length === 0)
-    //         alert("Nessura riga inserita");
-    //     else {
-    //         rows.forEach(async (row) => {
-    //             try {
-    //                 const response = await fetch("http://192.168.1.29:5000/record/add", {
-    //                     method: "POST",
-    //                     headers: {
-    //                         "Content-Type": "application/json",
-    //                     },
-    //                     body: JSON.stringify(row),
-    //                 });
-    //                 if (!response.ok) {
-    //                     const message = `An error occurred: ${response.statusText}`;
-    //                     window.alert(message);
-    //                     return;
-    //                 }
-    //                 setFormData({
-    //                     tipdoc: 'INVEN',
-    //                     datadoc: getCurrentDate(),
-    //                     codart: '',
-    //                     unimis: 'n.',
-    //                     quanti: 1,
-    //                     codmat: ''
-    //                 });
-    //                 setIsTestataSave(false)
-    //                 setRows([]);
-    //                 alert("Documento Creato!");
-    //             } catch (error) {
-    //                 console.error("Errore nella richiesta post:", error);
-    //                 window.alert(error);
-    //                 return;
-    //             };
-    //         });
-    //         navigate('/');
-    //     }
-    // };
     const postData = async (row) => {
         try {
             const response = await fetch("http://192.168.1.29:5000/record/add", {
@@ -155,174 +110,169 @@ const CreateDocument = () => {
         }
     };
 
-        const salvaTestata = () => {
-            setIsTestataSave(true)
-        }
-
-        const artHandler = () => {
-            setShow(!show)
-        }
-
-        const onArtSelect = (selected) => {
-            setFormData({
-                ...formData,
-                codart: selected.CACODART,
-                desc: selected.CADESART,
-            })
-            setCurrentArt(selected)
-        }
-
-        // const onArtSearch = (e) => {
-        //     const {value} = e.target;
-        //     console.log(value)
-        // }
-
-        const addRowHandler = (e) => {
-            e.preventDefault();
-            const newRecord = {
-                ...formData,
-                serial: newSerial,
-                magpar: '1026',
-                insuser: '690a10eb6bd3636a',
-                rownum: (rows.length + 1) * 10,
-            }
-            setRows(prevRows => [...prevRows, newRecord]);
-            // Resetta il formData
-            setFormData({
-                ...formData,
-                quanti: 1,
-                codmat: 'NULL',
-            });
-            alert("Riga Aggiunta")
-            if (rows.length === 0)
-                setIsTestataSave(true)
-        }
-
-        const onDelete = (rownum) => {
-            // Filtra gli elementi escludendo quello con rownum uguale a rownumToDelete
-            const updatedRows = rows.filter(row => row.rownum !== rownum);
-            // Aggiorna lo stato con il nuovo array di elementi
-            setRows(updatedRows);
-            resetByUpdate();
-            alert("Riga eliminata");
-        }
-
-        const onUpdate = (rownum) => {
-            setUpdate({ ...update, updating: true, rownum: rownum })
-            const rowByUpdate = rows.find(row => row.rownum === rownum);
-            setFormData({
-                ...formData,
-                codmat: rowByUpdate.codmat,
-                quanti: rowByUpdate.quanti,
-                codart: rowByUpdate.codart,
-                desc: rowByUpdate.desc
-            })
-        }
-
-        const resetByUpdate = () => {
-            setFormData({
-                ...formData,
-                quanti: 1,
-                codmat: 'NULL',
-                codart: '',
-                desc: '',
-            });
-            setUpdate({ ...update, updating: false, rownum: 0 })
-        }
-
-        const updateRow = () => {
-            // Crea un nuovo array aggiornando l'elemento con il rownum corrispondente
-            const updatedRows = rows.map(row => {
-                // Se rownum corrisponde, aggiorna l'elemento con i dati da formData
-                if (row.rownum === update.rownum) {
-                    return {
-                        ...row,
-                        codmat: formData.codmat,
-                        quanti: formData.quanti,
-                        codart: formData.codart,
-                        desc: formData.desc
-                    };
-                }
-                // Altrimenti, lascia l'elemento invariato
-                return row;
-            });
-            // Aggiorna lo stato con il nuovo array di elementi
-            setRows(updatedRows);
-            // Resetta il formData
-            resetByUpdate();
-            alert("Riga Aggiornata");
-        }
-
-        return (
-            <Container className='mt-3'>
-                <div className='my-3 d-flex justify-content-between'>
-                    <Link to="/"><Button variant='secondary'>Home</Button></Link>
-                    <h2>Nuovo Documento</h2>
-                </div>
-                <Form onSubmit={addRowHandler}>
-                    <Form.Group controlId="tipdoc">
-                        <Form.Label className='custom-label mt-3'>Tipo Documento</Form.Label>
-                        <Form.Control as="select" name="tipdoc" value={formData.tipdoc} onChange={handleChange} disabled={isTestataSave}>
-                            <option value="INVEN">Inventario</option>
-                        </Form.Control>
-                    </Form.Group>
-
-                    <Form.Group controlId="datadoc">
-                        <Form.Label className='custom-label mt-3'>Data Documento</Form.Label>
-                        <Form.Control required type="date" name="datadoc" value={formData.datadoc} onChange={handleChange} disabled={isTestataSave} />
-                    </Form.Group>
-                    {!isTestataSave && <div>
-                        <Button className="mt-3" variant="success" onClick={salvaTestata}>
-                            Conferma Testata
-                        </Button>
-                    </div>}
-                    <Form.Group controlId="codart">
-                        <Form.Label className='custom-label mt-3'>Articolo</Form.Label>
-                        <div className='d-flex'>
-                            <Form.Control placeholder="Cerca..." type="text" name="search" value={formData.search} onChange={handleChange} />
-                            <Button onClick={artHandler}><FontAwesomeIcon icon={faSearch} /></Button>
-                        </div>
-                        <Form.Control placeholder="Codice" required type="text" name="codart" defaultValue={formData.codart} readOnly />
-                        <Form.Control placeholder="Descrizione" type="text" name="desc" defaultValue={formData.desc} readOnly />
-                        {show && <Articoli show={show} handleClose={artHandler} handleArticoloSelect={onArtSelect} searchValue={formData.search}></Articoli>}
-                    </Form.Group>
-
-                    <Form.Group controlId="unimis">
-                        <Form.Label className='custom-label mt-3'>Unità di Misura</Form.Label>
-                        <Form.Control required as="select" name="unimis" value={formData.unimis} onChange={handleChange}>
-                            <option value="pz">Pz</option>
-                        </Form.Control>
-                    </Form.Group>
-
-                    <Form.Group controlId="quanti">
-                        <Form.Label className='custom-label mt-3'>Quantità</Form.Label>
-                        <Form.Control required type="number" name="quanti" value={formData.quanti} onChange={handleChange} />
-                    </Form.Group>
-
-                    {false && <Form.Group controlId="codmat">
-                        <Form.Label className='custom-label mt-3'>Codice Matricola</Form.Label>
-                        <Form.Control required type="text" name="codmat" value={formData.codmat} onChange={handleChange} />
-                    </Form.Group>}
-
-                    <div className='d-flex justify-content-between'>
-                        {!update.updating && <Button className="mt-3" variant="primary" type="submit">
-                            Aggiungi Riga
-                        </Button>}
-                        {update.updating && <Button onClick={() => { updateRow() }} className="mt-3" variant="success">
-                            Salva Modifiche
-                        </Button>}
-                        {!update.updating && <Button onClick={() => { handleSubmit() }} className="mt-3" variant="warning">
-                            Salva Documento
-                        </Button>}
-                        {update.updating && <Button onClick={() => resetByUpdate()} className="mt-3" variant="danger">
-                            Annulla
-                        </Button>}
-                    </div>
-                    <RowsList rows={rows} handleDelete={onDelete} handleUpdate={onUpdate} />
-                </Form>
-            </Container>
-        );
-
+    const salvaTestata = () => {
+        setIsTestataSave(true)
     }
 
-    export default CreateDocument
+    const artHandler = () => {
+        setShow(!show)
+    }
+
+    const onArtSelect = (selected) => {
+        setFormData({
+            ...formData,
+            codart: selected.CACODART,
+            desc: selected.CADESART,
+        })
+        setCurrentArt(selected)
+    }
+
+    const addRowHandler = (e) => {
+        e.preventDefault();
+        const newRecord = {
+            ...formData,
+            serial: newSerial,
+            magpar: '1026',
+            insuser: '690a10eb6bd3636a',
+            rownum: (rows.length + 1) * 10,
+        }
+        setRows(prevRows => [...prevRows, newRecord]);
+        // Resetta il formData
+        setFormData({
+            ...formData,
+            quanti: 1,
+            codmat: 'NULL',
+        });
+        alert("Riga Aggiunta")
+        if (rows.length === 0)
+            setIsTestataSave(true)
+    }
+
+    const onDelete = (rownum) => {
+        // Filtra gli elementi escludendo quello con rownum uguale a rownumToDelete
+        const updatedRows = rows.filter(row => row.rownum !== rownum);
+        // Aggiorna lo stato con il nuovo array di elementi
+        setRows(updatedRows);
+        resetByUpdate();
+        alert("Riga eliminata");
+    }
+
+    const onUpdate = (rownum) => {
+        setUpdate({ ...update, updating: true, rownum: rownum })
+        const rowByUpdate = rows.find(row => row.rownum === rownum);
+        setFormData({
+            ...formData,
+            codmat: rowByUpdate.codmat,
+            quanti: rowByUpdate.quanti,
+            codart: rowByUpdate.codart,
+            desc: rowByUpdate.desc
+        })
+    }
+
+    const resetByUpdate = () => {
+        setFormData({
+            ...formData,
+            quanti: 1,
+            codmat: 'NULL',
+            codart: '',
+            desc: '',
+        });
+        setUpdate({ ...update, updating: false, rownum: 0 })
+    }
+
+    const updateRow = () => {
+        // Crea un nuovo array aggiornando l'elemento con il rownum corrispondente
+        const updatedRows = rows.map(row => {
+            // Se rownum corrisponde, aggiorna l'elemento con i dati da formData
+            if (row.rownum === update.rownum) {
+                return {
+                    ...row,
+                    codmat: formData.codmat,
+                    quanti: formData.quanti,
+                    codart: formData.codart,
+                    desc: formData.desc
+                };
+            }
+            // Altrimenti, lascia l'elemento invariato
+            return row;
+        });
+        // Aggiorna lo stato con il nuovo array di elementi
+        setRows(updatedRows);
+        // Resetta il formData
+        resetByUpdate();
+        alert("Riga Aggiornata");
+    }
+
+    return (
+        <Container className='mt-3'>
+            <div className='my-3 d-flex justify-content-between'>
+                <Link to="/"><Button variant='secondary'>Home</Button></Link>
+                <h2>Nuovo Documento</h2>
+            </div>
+            <Form onSubmit={addRowHandler}>
+                <Form.Group controlId="tipdoc">
+                    <Form.Label className='custom-label mt-3'>Tipo Documento</Form.Label>
+                    <Form.Control as="select" name="tipdoc" value={formData.tipdoc} onChange={handleChange} disabled={isTestataSave}>
+                        <option value="INVEN">Inventario</option>
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId="datadoc">
+                    <Form.Label className='custom-label mt-3'>Data Documento</Form.Label>
+                    <Form.Control required type="date" name="datadoc" value={formData.datadoc} onChange={handleChange} disabled={isTestataSave} />
+                </Form.Group>
+                {!isTestataSave && <div>
+                    <Button className="mt-3" variant="success" onClick={salvaTestata}>
+                        Conferma Testata
+                    </Button>
+                </div>}
+                <Form.Group controlId="codart">
+                    <Form.Label className='custom-label mt-3'>Articolo</Form.Label>
+                    <div className='d-flex'>
+                        <Form.Control placeholder="Cerca..." type="text" name="search" value={formData.search} onChange={handleChange} />
+                        <Button onClick={artHandler}><FontAwesomeIcon icon={faSearch} /></Button>
+                    </div>
+                    <Form.Control placeholder="Codice" required type="text" name="codart" defaultValue={formData.codart} disabled/>
+                    <Form.Control placeholder="Descrizione" type="text" name="desc" defaultValue={formData.desc} readOnly disabled/>
+                    {show && <Articoli show={show} handleClose={artHandler} handleArticoloSelect={onArtSelect} searchValue={formData.search}></Articoli>}
+                </Form.Group>
+
+                <Form.Group controlId="unimis">
+                    <Form.Label className='custom-label mt-3'>Unità di Misura</Form.Label>
+                    <Form.Control required as="select" name="unimis" value={formData.unimis} onChange={handleChange}>
+                        <option value="pz">Pz</option>
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId="quanti">
+                    <Form.Label className='custom-label mt-3'>Quantità</Form.Label>
+                    <Form.Control required type="number" name="quanti" value={formData.quanti} onChange={handleChange} />
+                </Form.Group>
+
+                {false && <Form.Group controlId="codmat">
+                    <Form.Label className='custom-label mt-3'>Codice Matricola</Form.Label>
+                    <Form.Control required type="text" name="codmat" value={formData.codmat} onChange={handleChange} />
+                </Form.Group>}
+
+                <div className='d-flex justify-content-between'>
+                    {!update.updating && <Button className="mt-3" variant="primary" type="submit">
+                        Aggiungi Riga
+                    </Button>}
+                    {update.updating && <Button onClick={() => { updateRow() }} className="mt-3" variant="success">
+                        Salva Modifiche
+                    </Button>}
+                    {!update.updating && <Button onClick={() => { handleSubmit() }} className="mt-3" variant="warning">
+                        Salva Documento
+                    </Button>}
+                    {update.updating && <Button onClick={() => resetByUpdate()} className="mt-3" variant="danger">
+                        Annulla
+                    </Button>}
+                </div>
+                <RowsList rows={rows} handleDelete={onDelete} handleUpdate={onUpdate} />
+            </Form>
+        </Container>
+    );
+
+}
+
+export default CreateDocument
